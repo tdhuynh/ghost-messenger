701dfc32e878ae0af9f92ee25565dd1b5acb30f1
tdhuynh <tommyhuynh93@gmail.com>
2017-01-20 13:56:58 -0500

    created index view

--- messenger/views.py
+++ messenger/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+
+
+IndexView(TemplateView):
+    template_name = "index.html"
